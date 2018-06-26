206ddabf3b44fc8fec380c5b8f04dfe57502e201
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 23:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function findMinAndRemove(array){
   let min = array[0];
   let minIndex = 0;
-  for (i=0; i > array.length; i++)
+  for (i=0; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
 }
 
 function insertionSort(array){
