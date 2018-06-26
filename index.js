6ec0c1c7bf270c0acf464c2e9b99319e22a19ef3
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 23:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function findMinAndRemove(array){
   let min = array[0];
   let minIndex = 0;
-  for (i=0; i < array.length; i++) {
+  for (let i=0; i < array.length; i++) {
     if (min > array[i]) {
       min = array[i];
       minIndex = i;
